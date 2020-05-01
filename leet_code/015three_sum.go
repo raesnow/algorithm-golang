50bcc513c07c3454aebaaf529b3a73fc677b5791
guoxuerong <xuerongguo_rae@qq.com>
2020-05-01 23:03:24 +0800

    feat: leet code 015

--- leet_code/015three_sum.go
+++ leet_code/015three_sum.go
@@ -1,5 +1,7 @@
 package main
 
+import sort2 "sort"
+
 /*
 Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
@@ -18,6 +20,9 @@ A solution set is:
 ]
 */
 
+// threeSum
+// time: O(n*n)
+// storage: O(n*n)
 func threeSum(nums []int) [][]int {
 	if len(nums) < 3 {
 		return nil
@@ -75,5 +80,49 @@ func deDuplicate(result [][]int, threes []int) [][]int {
 		}
 	}
 	result = append(result, threes)
+	return result
+}
+
+// threeSum1
+// time: O(n*n)
+// storage: O(n*n)
+func threeSum1(nums []int) [][]int {
+	if len(nums) < 3 {
+		return nil
+	}
+
+	sort2.Ints(nums)
+	result := make([][]int, 0)
+
+	for i := 0; i < len(nums); i++ {
+		// 如果第一个数就大于0，则无解
+		if nums[i] > 0 {
+			return result
+		}
+
+		// 跳过相同的，避免重复解
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		left := i + 1
+		right := len(nums) - 1
+		for left < right {
+			if nums[i]+nums[left]+nums[right] == 0 {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+				for ; left < right && nums[left] == nums[left+1]; left++ {
+				}
+				for ; left < right && nums[right] == nums[right-1]; right-- {
+				}
+				left++
+				right--
+			} else if nums[i]+nums[left]+nums[right] > 0 {
+				right--
+			} else {
+				left++
+			}
+		}
+	}
+
 	return result
 }
--- leet_code/015three_sum_test.go
+++ leet_code/015three_sum_test.go
@@ -28,3 +28,27 @@ func Test_threeSum(t *testing.T) {
 		})
 	}
 }
+
+func Test_threeSum1(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "test1",
+			args: args{nums: []int{-1, 0, 1, 2, -1, -4}},
+			want: [][]int{{-1, 0, 1}, {-1, -1, 2}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSum1(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("threeSum1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
